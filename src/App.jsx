cf7b25da7bb06b8979d059efb3ef11567cf26bff
ahmed alsayeh <sayehxp@gmail.com>
2023-10-25 16:44:18 +0300

    added navbar by sayeh

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,11 @@
-import { useState } from 'react'
-
-import './App.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-
-
+import Nav_Bar from './Components/Nav_Bar/Nav_Bar'
 function App() {
 
 
   return (
   <>
-{/* by sayeh */}
-<Navbar bg="dark" data-bs-theme="dark">
-  <Container>
-    <Navbar.Brand href="#home" className='m-2'>Adidas</Navbar.Brand>
-    <Nav className="me-auto">
-      <Nav.Link href="#home">Home</Nav.Link>
-      <Nav.Link href="#features">Features</Nav.Link>
-      <Nav.Link href="#pricing">Pricing</Nav.Link>
-    </Nav>
-  </Container>
-</Navbar>
 
+<Nav_Bar/>
 
   </>
   )
